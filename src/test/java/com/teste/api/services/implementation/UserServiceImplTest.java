0e391871993d5c3d68316509bf13d09b0a4dc534
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-02-02 15:41:06 -0300

    Teste Delete, caso Exception

--- src/test/java/com/teste/api/services/implementation/UserServiceImplTest.java
+++ src/test/java/com/teste/api/services/implementation/UserServiceImplTest.java
@@ -136,6 +136,17 @@ class UserServiceImplTest {
         verify(repository, times(1)).deleteById(anyInt());
     }
 
+    @Test
+    void deleteWithObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+        try {
+            service.delete(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertTrue("Objeto não encontrado".contains(ex.getMessage()));
+        }
+    }
+
     private void startUser() {
         user = new User(ID, NAME, EMAIL, PASSWORD);
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
