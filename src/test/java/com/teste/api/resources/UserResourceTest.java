ea6a7778bc594b4b5c2ed7e919d615a25514de56
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-02-02 15:50:05 -0300

    Criacao da classe UserResourceTest

---
+++ src/test/java/com/teste/api/resources/UserResourceTest.java
@@ -0,0 +1,64 @@
+package com.teste.api.resources;
+
+import com.teste.api.domain.User;
+import com.teste.api.domain.dto.UserDTO;
+import com.teste.api.services.implementation.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final Integer ID = 1;
+    public static final String NAME = "Gabriel Teste";
+    public static final String EMAIL = "gabrielteste@gmail.com";
+    public static final String PASSWORD = "123";
+
+    private User user;
+    private UserDTO userDTO;
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+
+    }
+}
