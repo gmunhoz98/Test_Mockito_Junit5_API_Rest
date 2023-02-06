d6d52502255202621be98e5c3a2ae29eeec1a356
Gabriel Munhoz <GabrielMunhoz@UNION.icarotech.com>
2023-02-06 08:53:19 -0300

    Teste DataIntegrity na classe ResourceExceptionHandlerTest

--- src/test/java/com/teste/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/com/teste/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.teste.api.resources.exceptions;
 
+import com.teste.api.services.exceptions.DataIntegratyViolationException;
 import com.teste.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
@@ -37,6 +39,18 @@ class ResourceExceptionHandlerTest {
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler.dataIntegrityViolationException(new DataIntegratyViolationException("Email já cadastrado"), new MockHttpServletRequest());
+        response.getBody().getPath();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Email já cadastrado", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 }
